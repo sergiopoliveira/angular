8af22a924354bb7b6d4f8ee3235ed471ae3cf644
sergiopoliveira <sergiopoliveira@outlook.com>
2020-07-04 17:22:30 +0100

    Interpolation

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -7,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelloComponent implements OnInit {
 
+  myVariable = "variable"
+  bitwiseOR = 2 | 5 // 010 101 = 0 | 1 = 1
+  i = 0
+
   constructor() { }
+
+  // not recommended to use functions in HTML interpolation
+  // (called multiple times)
+  doSomeHeavyTask() {
+    console.log(`Called ${this.i++} times`)
+  }
 
   ngOnInit(): void {
   }
