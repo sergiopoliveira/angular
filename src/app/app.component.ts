5df5d1369656db5e68507ab1f387242ff39f7094
sergiopoliveira <sergiopoliveira@outlook.com>
2020-07-04 16:11:17 +0100

    decorator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,18 @@
 import { Component } from '@angular/core';
 
+function log(target, name, descriptor) {
+  console.log(target.name, descriptor)
+  // store the original function in a variable
+  const original = descriptor.value
+
+  // do some manipulations with descriptor.value
+  descriptor.value = function () {
+    console.log("This function was hacked!")
+  }
+  // return the descriptor
+  return descriptor
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +20,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+
+  constructor() {
+    this.aSimpleMethod();
+  }
+
+  @log
+  aSimpleMethod() {
+    console.log("Hey there!")
+  }
+
 }
