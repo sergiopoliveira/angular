367f09b10bc03116cc42a1cc27bfa7f357916b06
sergiopoliveira <sergiopoliveira@outlook.com>
2020-07-07 12:12:26 +0100

    Login App - Part 2

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private Http: HttpClient) { }
 
-  getUserDetails() {
+  getUserDetails(username, password) {
     // post these details to API server, return user info if correct
+    return this.Http.post('api/auth.php', {
+      username,
+      password
+    }).subscribe(data => {
+      console.log(data, " is what we got from the server")
+    })
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private Auth: AuthService) { }
 
   ngOnInit(): void {
   }
 
-  loginUser(event){
+  loginUser(event) {
     event.preventDefault()
+    const target = event.target
+    const username = target.querySelector('#username').value
+    const password = target.querySelector('#password').value
+    this.Auth.getUserDetails(username, password)
     console.log(event)
   }
 
